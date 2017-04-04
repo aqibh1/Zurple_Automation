4aa3f5ddef78cc62c638f1f61403a9bfc8dbc082
Vladimir <vladimir.a.zotov@gmail.com>
2017-04-04 17:19:52 +0300

    #1425 - Created a test for Agent Create page

--- src/test/java/com/zurple/my/AgentCreatePageTest.java
+++ src/test/java/com/zurple/my/AgentCreatePageTest.java
@@ -1,5 +1,13 @@
 package com.zurple.my;
 
+import java.util.UUID;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
 /**
  * todo
  *
@@ -22,4 +30,29 @@ public class AgentCreatePageTest
     public void clearPage(){
         page=null;
     };
+
+
+    @Test
+    public void testCreationNewAdmin(){
+        assertTrue(getPage().checkAgentEditForm());
+        String uuid = UUID.randomUUID().toString();
+        String adminFirstName = uuid.substring(0,5)+"test";
+        String adminLastName = "Admin";
+        String adminEmail = adminFirstName+"_"+adminLastName+"@test.com";
+        String adminAltEmail = adminFirstName+"_"+adminLastName+"_alt@test.com";
+        String adminPassword = uuid.substring(0,5);
+        getPage().getAgentCreateForm().setInputValue("first_name",adminFirstName);
+        getPage().getAgentCreateForm().setInputValue("last_name",adminLastName);
+        getPage().getAgentCreateForm().setInputValue("email",adminEmail);
+        getPage().getAgentCreateForm().setInputValue("password",adminPassword);
+        getPage().getAgentCreateForm().setInputValue("password_confirm",adminPassword);
+
+        getPage().getAgentCreateForm().submit();
+
+        AssertJUnit.assertTrue(getPage().getAgentCreateForm().checkAgentCreateWarningExists());
+        AssertJUnit.assertTrue(getPage().getAgentCreateForm().checkAgentCreateWarningVisible());
+        getPage().getAgentCreateForm().getAgentCreateWarning().clickOkButton();
+
+        assertFalse(getEnvironment().getAdminByEmail(adminEmail).getBillingAccessFlag());
+    }
 }
