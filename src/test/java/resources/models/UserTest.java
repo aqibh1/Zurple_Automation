23dfa3c7807c44a741aace84b43d051548d8d512
Vladimir <vladimir.a.zotov@gmail.com>
2018-07-23 11:55:30 +0300

    #2442 - Added user status automation test for case client->prospect1

--- src/test/java/resources/models/UserTest.java
+++ src/test/java/resources/models/UserTest.java
@@ -105,4 +105,28 @@ public class UserTest extends AbstractTest
                         
     }
 
+    @Test
+    public void testClientUserBecomesProspect1(){
+        User lastRegisteredUser = getEnvironment().getLastRegisteredUser();
+
+        Date d = new Date();
+        d.setTime(d.getTime() - 31L * 24 * 60 * 60 * 1000);
+        lastRegisteredUser.setCreateDatetime(d);
+        lastRegisteredUser.setUserStatus("client");
+        lastRegisteredUser.save();
+
+        List<SessionUser> sessions = getEnvironment().getUserSessions(lastRegisteredUser);
+        for (SessionUser session: sessions) {
+            session.setSessionEnd(d);
+            session.save();
+        }
+
+        Set<UserAlert> alerts = lastRegisteredUser.getUserAlerts();
+        for (UserAlert alert: alerts) {
+            alert.setUserAlertTriggered(d);
+            alert.save();
+        }
+
+    }
+
 }
