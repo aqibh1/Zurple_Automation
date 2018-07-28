a19281ebb3a6cc6c7229f6eeb2bef987688e61cc
vzotov <vladimir@yojimbocorp.com>
2018-07-28 12:32:08 +0300

    #2458 - Refactored hybernate to use db server data from config

--- src/main/java/resources/orm/hibernate/HibernateUtil.java
+++ src/main/java/resources/orm/hibernate/HibernateUtil.java
@@ -3,15 +3,25 @@ package resources.orm.hibernate;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import resources.ConfigReader;
 
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
+            ConfigReader configReader = ConfigReader.load();
             // Create the SessionFactory from hibernate.cfg.xml
-            return new AnnotationConfiguration().configure().buildSessionFactory();
+            Configuration cfg = new AnnotationConfiguration().configure();
+            cfg.setProperties(System.getProperties());
+            cfg.getProperties().setProperty("hibernate.connection.url","jdbc:mysql://"+configReader.getPropertyByName("mysql_host")+":"+configReader.getPropertyByName("mysql_port")+"/"+configReader.getPropertyByName("mysql_db")+"?zeroDateTimeBehavior=convertToNull");
+            cfg.getProperties().setProperty("hibernate.connection.username",configReader.getPropertyByName("mysql_user"));
+            cfg.getProperties().setProperty("hibernate.connection.password",configReader.getPropertyByName("mysql_pass"));
+            cfg.addResource("hibernate.cfg.xml");
+            SessionFactory sessionFactory = cfg.buildSessionFactory();
+            return sessionFactory;
 
         }
         catch (Throwable ex) {
