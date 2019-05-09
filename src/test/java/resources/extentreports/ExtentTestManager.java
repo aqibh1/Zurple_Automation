443474c9c7cafdb5140d38ad110e524b99e1839a
Aqib Dar <aqib.dar@z57.com>
2019-05-09 16:14:05 +0500

    Corrected the path of the image.

--- src/test/java/resources/extentreports/ExtentTestManager.java
+++ src/test/java/resources/extentreports/ExtentTestManager.java
@@ -39,12 +39,13 @@ public class ExtentTestManager {
                     String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
      TakesScreenshot ts = (TakesScreenshot) driver;
      File source = ts.getScreenshotAs(OutputType.FILE);
+     String relitive_path = "\\target\\surefire-reports\\screenshots\\"+screenshotName+dateName+".png";
                     //after execution, you could see a folder "FailedTestsScreenshots" under src folder
-     String destination = System.getProperty("user.dir") + "\\target\\surefire-reports\\screenshots\\"+screenshotName+dateName+".png";
+     String destination = System.getProperty("user.dir") + relitive_path;
      File finalDestination = new File(destination);
      FileUtils.copyFile(source, finalDestination);
                     //Returns the captured file path
-     return destination;
+     return relitive_path;
     }
    
 }
