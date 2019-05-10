c01ecfb7b87d7593693fdeb6a9766d9f60899064
Aqib Dar <aqib.dar@z57.com>
2019-05-10 14:10:10 +0500

    Corrected the path of the image.

--- src/test/java/resources/extentreports/ExtentTestManager.java
+++ src/test/java/resources/extentreports/ExtentTestManager.java
@@ -53,7 +53,7 @@ public class ExtentTestManager {
      Path pathRelative = pathBase.relativize(pathAbsolute);
      System.out.println(pathRelative);
      
-     return finalDestination.getPath();
+     return pathRelative.toString();
     }
    
 }
