07a9d17f68aaf01936e19e9977dfb51de1fc5da7
Aqib Dar <aqib.dar@z57.com>
2019-10-09 15:56:43 +0500

    Added page test class for database test cases

---
+++ src/test/java/com/z57/propertypulse/PageTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.z57.propertypulse;
+
+import resources.AbstractPageTest;
+import resources.interfaces.TestHavingHeader;
+import resources.interfaces.UsingPage;
+
+/**
+ * @author adar
+ *
+ */
+public abstract class PageTest extends AbstractPageTest  implements UsingPage, TestHavingHeader {
+	
+
+}
