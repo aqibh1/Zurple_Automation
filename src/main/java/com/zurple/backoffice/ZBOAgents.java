b9486e51f7dbc64c0dea3edac7d6552c8e9e1904
Aaqib Habib <habibaaq@CS.NET>
2020-02-10 19:24:25 +0500

    intiating classes and finding page label

---
+++ src/main/java/com/zurple/backoffice/ZBOAgents.java
@@ -0,0 +1,29 @@
+package com.zurple.backoffice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.zurple.my.Page;
+
+import resources.utility.ActionHelper;
+
+public class ZBOAgents extends Page{
+
+	@FindBy(className="col-sm-4")
+	WebElement manage_agents_label;
+	
+	public ZBOAgents() {
+		
+	}
+	
+	public ZBOAgents(WebDriver pWebDriver) {
+		driver = pWebDriver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public boolean getPageTitle(){
+		return ActionHelper.waitForElementToBeVisible(driver, manage_agents_label, 30);
+	}
+}
