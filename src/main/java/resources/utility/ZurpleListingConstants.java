e372c4a49aa9ad6de4b7a11cc80f8c4833bf0e38
Aqib Dar <aqib.dar@z57.com>
2020-02-19 18:33:55 +0500

    Added listing constants

--- src/main/java/resources/utility/ZurpleListingConstants.java
+++ src/main/java/resources/utility/ZurpleListingConstants.java
@@ -4,5 +4,9 @@ public class ZurpleListingConstants {
 	public static final String zurple_staging_listing = "/CA/San_Diego/34141976";
 	public static final String zurple_production_listing = "email_subject_line";
 	public static final String zurple_mls_listing_id = "190020375";
+	public static final String zurple_bo_property_url_staging = "/property/34227497";
+	public static final String zurple_bo_property_url_prod = "/property/46569792";
+	public static final String zurple_website_property_url_staging = "/CA/Chula_Vista/34227497";
+	public static final String zurple_website_property_url_prod = "/CA/Santee/46569792";
 
 }
