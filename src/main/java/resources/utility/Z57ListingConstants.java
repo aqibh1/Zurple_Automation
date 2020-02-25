d32b45d2f32de1fd35506bb0b1d095a3b18686c9
Aqib Dar <aqib.dar@z57.com>
2020-02-25 16:52:44 +0500

    Updated the listing URL

---
+++ src/main/java/resources/utility/Z57ListingConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package resources.utility;
+
+/**
+ * @author adar
+ *
+ */
+public class Z57ListingConstants {
+	
+	public static final String z57_listing = "/idx/listings/cws/1098/190050394/chula-vista-san-diego-county-ca-91910";
+
+}
