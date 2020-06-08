f81a21ae64065d03abef4a611dc6ce21be1191c0
Aqib Dar <aqib.dar@z57.com>
2020-06-08 19:53:11 +0500

    Added method to verify text after regular intervals.

--- src/main/java/resources/utility/ActionHelper.java
+++ src/main/java/resources/utility/ActionHelper.java
@@ -946,4 +946,19 @@ public class ActionHelper {
 		   }
 		   return element!=null;
 	   }
+	   public static boolean verifyTextAfterRegularIntervals(WebDriver pWebDriver,WebElement pXpath,String pTextToVerify, int pTotalAttempts) {
+		  boolean isVerified = false;
+		   int counter = 0;
+		   while (counter<pTotalAttempts && !isVerified) {
+			   try {
+				   isVerified = getText(pWebDriver, pXpath).equalsIgnoreCase(pTextToVerify);
+			   }catch(Exception ex) {
+				   AutomationLogger.error("Unable to get dynamic webelement for xpath "+pXpath);
+				   AutomationLogger.error(ex.getMessage());
+				   pWebDriver.navigate().refresh();
+				   staticWait(60);
+			   }
+		   }
+		   return isVerified;
+	   }
 }
