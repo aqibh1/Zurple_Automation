bdc4dd942aa5bc8e3ecb0e2f8de8731c455b6907
Aqib Dar <aqib.dar@z57.com>
2020-08-18 15:28:37 +0500

    #4168 Added twitter status and photo test cases.

--- src/test/java/com/restapi/RestAPITest.java
+++ src/test/java/com/restapi/RestAPITest.java
@@ -7,15 +7,55 @@ import java.io.IOException;
 import java.net.ProtocolException;
 
 import org.apache.http.client.ClientProtocolException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import resources.AbstractAPIRestTest;
+import resources.AbstractPage;
+import resources.AbstractPageTest;
+import resources.EnvironmentFactory;
 import resources.utility.AutomationLogger;
 
 /**
  * @author adar
  *
  */
-public class RestAPITest extends AbstractAPIRestTest {
+public abstract class RestAPITest extends AbstractPageTest {
 
+	private String restApiBaseUrl = "";
+	public abstract boolean validateMapResp(RestResponse httpCallResp) throws Exception;
+	
+	public String getBaseUrl() {
+		EnvironmentFactory.configReader.load();
+		String l_project = System.getProperty("project");
+		if(l_project.equalsIgnoreCase("zurple")) {
+			restApiBaseUrl = EnvironmentFactory.configReader.getPropertyByName("zurple_bo_base_url");
+		}
+		return restApiBaseUrl;
+	}
+
+	@Override
+	public AbstractPage getPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearPage() {
+		// TODO Auto-generated method stub
+		
+	}
+	public String getValidationAction(JSONObject pDataObject, String pClassName) {
+		String lValidationAction ="";
+		JSONArray jArray = pDataObject.getJSONArray("validation");
+		for(int i=0;i<jArray.length();i++) {
+			JSONObject jObject = jArray.getJSONObject(i);
+			if(jObject.getString("key").equalsIgnoreCase(pClassName)) {
+				lValidationAction = jObject.getString("value");
+				break;
+			}
+		}
+		return lValidationAction;
+	}
 	
 }
---
+++ src/test/java/com/restapi/RestValidationAction.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.restapi;
+
+/**
+ * @author adar
+ *
+ */
+public class RestValidationAction {
+	public static final String SUCCESS = "SUCCESS";
+	public static final String FAILURE = "FAILURE";
+	public static final String SAVE = "SAVE";
+	public static final String VALID = "VALID";
+	public static final String INVALID = "INVALID";
+	public static final String CREATE = "CREATE";
+	public static final Object FOUND = "FOUND";
+	
+}
