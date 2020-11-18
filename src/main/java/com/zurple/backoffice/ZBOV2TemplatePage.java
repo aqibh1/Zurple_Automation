f4c3a7db494619dfe3500ea2806a236ed823db14
Aaqib Habib <habibaaq@CS.NET>
2020-11-18 20:11:55 +0500

    adding selectors and page implementation

--- src/main/java/com/zurple/backoffice/ZBOV2TemplatePage.java
+++ src/main/java/com/zurple/backoffice/ZBOV2TemplatePage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.zurple.my.Page;
 
+import resources.utility.ActionHelper;
+
 public class ZBOV2TemplatePage extends Page {
 
 	@FindBy(id="domain_name")
@@ -27,6 +29,9 @@ public class ZBOV2TemplatePage extends Page {
 	@FindBy(id="update")
 	WebElement update_sitemgr;
 	
+	@FindBy(id="customize_v2_tx_url")
+	WebElement customized_url;
+	
 	ZBOV2TemplatePage(){
 		
 	}
@@ -34,6 +39,34 @@ public class ZBOV2TemplatePage extends Page {
 	ZBOV2TemplatePage(WebDriver pWebDriver){
 		driver = pWebDriver;
 		PageFactory.initElements(driver, this);
+	}
+	
+	public String getDomainName() {
+		return ActionHelper.getText(driver, domain_name);
+	}
+	
+	public boolean clickV2Checkbox() {
+		return ActionHelper.Click(driver, v2_checkbox);
+	}
+	
+	public String getValidationMessage() {
+		return ActionHelper.getText(driver, validation_message);
+	}
+	
+	public boolean clickDeleteCity() {
+		return ActionHelper.Click(driver, city_del);
+	}
+	
+	public boolean clickAddCity() {
+		return ActionHelper.Click(driver, city_add);
+	}
+	
+	public boolean clickUpdate() {
+		return ActionHelper.Click(driver, update_sitemgr);
+	}
+	
+	public boolean clickCustomizedURL() {
+		return ActionHelper.Click(driver, customized_url);
 	}
 	
 }
--- src/main/java/com/zurple/website/ZWV2TemplateHomePage.java
+++ src/main/java/com/zurple/website/ZWV2TemplateHomePage.java
@@ -1,5 +1,69 @@
 package com.zurple.website;
 
-public class ZWV2TemplateHomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import us.zengtest1.Page;
+
+public class ZWV2TemplateHomePage extends Page{
+
+	@FindBy(xpath="//li[@class='tx_socialicons']/descendant::span[contains(text(),'facebook')]")
+	WebElement fb_icon;
+	
+	@FindBy(xpath="//li[@class='tx_socialicons']/descendant::span[contains(text(),'twitter')]")
+	WebElement tw_icon;
+	
+	@FindBy(xpath="//li[@class='tx_socialicons']/descendant::span[contains(text(),'youtube')]")
+	WebElement yt_icon;
+	
+	@FindBy(xpath="//div[@class='container']/descendant::h1[contains(text(),'Start  Your Home Search')]")
+	WebElement search_label;
+	
+	@FindBy(id="basic_city")
+	WebElement search_text;
+	
+	@FindBy(xpath="//input[@value='SEARCH']")
+	WebElement search_button;
+	
+	@FindBy(xpath="//input[@value='Advance Search']")
+	WebElement advance_search;
+	
+	@FindBy(className="blurb-label")
+	WebElement blurb_title;
+	
+	@FindBy(className="blurb-text")
+	WebElement blurb_text;
+	
+	@FindBy(xpath="//div[@id='footer_links']/descendant::a[@href='san_diego-real-estate']")
+	WebElement footer_link;
+	
+	public ZWV2TemplateHomePage(WebDriver pWebDriver) {
+		driver = pWebDriver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public ZWV2TemplateHomePage() {
+		
+	}
+	
+	@Override
+	public WebElement getHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WebElement getBrand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WebElement getTopMenu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
