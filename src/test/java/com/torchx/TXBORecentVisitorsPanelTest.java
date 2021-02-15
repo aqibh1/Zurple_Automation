f93c044021cb9b592ba248f4003baa6d680c1431
Aaqib Habib <habibaaq@CS.NET>
2021-02-15 11:57:17 +0500

    incorporated feedback

--- src/test/java/com/torchx/TXBORecentVisitorsPanelTest.java
+++ src/test/java/com/torchx/TXBORecentVisitorsPanelTest.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.testng.SkipException;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -24,7 +25,7 @@ public class TXBORecentVisitorsPanelTest extends PageTest {
 	private WebDriver driver;
 	TXBORecentVisitorsPanel page;
 	private JSONObject dataObject;
-	private JSONObject lDataObject;
+	private JSONObject lDataObject;	
 	
 	@Override
 	public AbstractPage getPage() {
@@ -48,15 +49,20 @@ public class TXBORecentVisitorsPanelTest extends PageTest {
 		return page;
 	}
 	
+	@BeforeTest
+	public void loginToBO() {
+		getPage();
+		if(!getLoginPage().doLogin(getTXBOUsername(), getTXBOPassword())) {
+			throw new SkipException("Skipping the test because [Login] pre-condition was failed.");
+		}
+	}
+	
 	@Test
 	@Parameters({"dataFile"})
 	public void testVerifyRecentVisitorsPanelHeader(String pDataFile) {
 		getPage();
 		dataObject = getDataFile(pDataFile);
-		if(!getLoginPage().doLogin(getTXBOUsername(), getTXBOPassword())) {
-			throw new SkipException("Skipping the test because [Login] pre-condition was failed.");
-		}
-		assertEquals(page.getPanelHeaderText(),dataObject.optString("panel_header"));
+		assertEquals(page.getPanelHeaderText(),dataObject.optString("panel_header"),"Panel Header is not found..");
 	}
 	
 	@Test
@@ -64,7 +70,7 @@ public class TXBORecentVisitorsPanelTest extends PageTest {
 	public void testVerifyRecentVisitorsPanelTooltip(String pDataFile) {
 		getPage();
 		dataObject = getDataFile(pDataFile);
-		assertEquals(page.getToolTipText(),dataObject.optString("tooltip_text"));
+		assertEquals(page.getToolTipText(),dataObject.optString("tooltip_text"),"Tooltip text is not valid..");
 	}
 	
 	@Test
@@ -72,17 +78,35 @@ public class TXBORecentVisitorsPanelTest extends PageTest {
 	public void testVerifyLeadFormat(String pDataFile) {
 		getPage();
 		lDataObject = getDataFile(pDataFile);
-		String txWebsite = getIsProd()?getTXProdWebsite():getTXStageWebsite();
-		ZWRegisterUserPage registerPage = new ZWRegisterUserPage(driver);
 		String lName = updateName(lDataObject.optString("tx_name"));
 		String lEmail = updateEmail(lDataObject.optString("email"));
-		// Pre-condition
+		String lPhone = lDataObject.optString("tx_phone");
+		registerUser(lName, lEmail, lPhone);
+		page=null;
+		getPage();
+		page.getLeadInfo();
+		assertEquals(page.leadInfoList.get(0).trim(),lName);
+		assertTrue(page.leadInfoList.get(1).trim().contains(lDataObject.optString("tx_phone")),"Unable to verify lead format..");
+	}
+	
+	@Test
+	public void testVerifyLeadVisitDate() {
+		page=null;
+		getPage();
+		page.getLeadInfo();
+		assertTrue(page.leadInfoList.get(2).trim().contains(getTodaysDateInPST(0,"MM/dd/YY")+" at"),"Unable to verify lead visit date..");
+	}
+	
+	public void registerUser(String lName, String lEmail, String lPhone) {
 		try {
+			String txWebsite = getIsProd()?getTXProdWebsite():getTXStageWebsite();
+			ZWRegisterUserPage registerPage = new ZWRegisterUserPage(driver);
+			// Pre-condition
 			ActionHelper.openUrlInCurrentTab(driver, txWebsite+"/register");
 			assertTrue(registerPage.isRegisterPage(),"Register page is not opened..");
 			assertTrue(registerPage.typeName(lName),"Unable to type name..");
 			assertTrue(registerPage.typeEmail(lEmail),"Unable to type email..");
-			if(!lDataObject.optString("tx_phone").isEmpty()) {
+			if(!lPhone.isEmpty()) {
 				assertTrue(registerPage.typePhone(lDataObject.optString("tx_phone")),"Unable to type phone..");
 			}
 			assertTrue(registerPage.isTermsAndCondCheckboxChecked(),"Terms and conditions checkbox is not checked..");
@@ -91,24 +115,10 @@ public class TXBORecentVisitorsPanelTest extends PageTest {
 		} catch(Exception e) {
 			throw new SkipException("Skipping the test: Unable to register lead..");
 		}
-		// Actual Test
-		page=null;
-		getPage();
-		page.getLeadInfo();
-		assertEquals(page.leadInfoList.get(0).trim(),lName);
-		assertTrue(page.leadInfoList.get(1).trim().contains(lDataObject.optString("tx_phone")));
-	}
-	
-	@Test
-	public void testVerifyLeadVisitDate() {
-		page=null;
-		getPage();
-		page.getLeadInfo();		
-		assertTrue(page.leadInfoList.get(2).trim().contains(getTodaysDate(0,"MM/dd/YY")+" at"));
 	}
 	
 	@AfterTest
 	public void closeBrowser() {
-		driver.quit();
+		closeCurrentBrowser();
 	}
 }
