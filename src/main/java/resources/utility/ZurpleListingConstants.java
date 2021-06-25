11bbf3be7b19179aa097759c746934c978731677
aqib.dar@z57.com <aqib.dar@z57.com>
2021-06-25 14:52:21 +0500

    Fixed the add property note test case

--- src/main/java/resources/utility/ZurpleListingConstants.java
+++ src/main/java/resources/utility/ZurpleListingConstants.java
@@ -5,10 +5,8 @@ public class ZurpleListingConstants {
 	public static final String zurple_production_listing = "/CA/San_Diego/60372300";
 	public static final String zurple_mls_listing_id = "200022516";
 	public static final String zurple_bo_property_url_staging = "/property/34388436";
-	public static final String zurple_bo_property_url_prod = "/property/53184654";
-	public static final String zurple_website_property_url_staging = "/CA/San_Diego/34388436";
-	public static final String zurple_website_property_url_prod = "/CA/San_Diego/53184654";
-	public static final String zurple_sapi_listing_id_prod ="56355937";
+	public static final String zurple_bo_property_url_prod = "/property/60372300";
+	public static final String zurple_sapi_listing_id_prod ="60372300";
 	public static final String zurple_sapi_listing_id_stage ="34388436";
 	public static final String zurple_prod_land_url = "https://www.zengtest2.us";
 }
