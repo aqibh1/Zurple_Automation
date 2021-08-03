daefd9652c020103cf13711ca1f7e54c66dc1d21
Muhammad Aaqib Habib <aaqib.habib@z57.com>
2021-08-03 07:48:11 -0700

    updating logic for social schedule video tests and updated listing ids

--- src/main/java/resources/utility/ZurpleListingConstants.java
+++ src/main/java/resources/utility/ZurpleListingConstants.java
@@ -1,8 +1,8 @@
 package resources.utility;
 
 public class ZurpleListingConstants {
-	public static final String zurple_staging_listing = "/CA/San_Diego/34388436";
-	public static final String zurple_production_listing = "/CA/San_Diego/60372300";
+	public static final String zurple_staging_listing = "/CA/San_Diego/35983584";
+	public static final String zurple_production_listing = "/CA/San_Diego/62538339";
 	public static final String zurple_mls_listing_id = "200022516";
 	public static final String zurple_bo_property_url_staging = "/property/34388436";
 	public static final String zurple_bo_property_url_prod = "/property/60372300";
