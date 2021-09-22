d6c6806e5f029d581c4a9590aec85a1d88569a8c
Muhammad Aaqib Habib <aaqib.habib@z57.com>
2021-09-22 05:37:26 -0700

    updated social listing ids

--- src/main/java/resources/utility/ZurpleListingConstants.java
+++ src/main/java/resources/utility/ZurpleListingConstants.java
@@ -6,7 +6,7 @@ public class ZurpleListingConstants {
 	public static final String zurple_mls_listing_id = "200022516";
 	public static final String zurple_bo_property_url_staging = "/property/34112838";
 	public static final String zurple_bo_property_url_prod = "/property/60372300";
-	public static final String zurple_sapi_listing_id_prod ="61611107";
-	public static final String zurple_sapi_listing_id_stage ="34388436";
+	public static final String zurple_sapi_listing_id_prod ="61032968";
+	public static final String zurple_sapi_listing_id_stage ="35587661";
 	public static final String zurple_prod_land_url = "https://www.zengtest2.us";
 }
