3271675ea3996cc445c92e0c781cc81634600e5e
aqib.dar@z57.com <aqib.dar@z57.com>
2021-11-05 15:01:14 +0500

    #5499 Code review feedback

--- src/test/java/com/restapi/autoprovision/ZBORestPostPackage.java
+++ src/test/java/com/restapi/autoprovision/ZBORestPostPackage.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -20,14 +19,11 @@ import com.restapi.RestAPITest;
 import com.restapi.RestContent;
 import com.restapi.RestRequest;
 import com.restapi.RestResponse;
-import com.restapi.RestValidationAction;
 
 import resources.EnvironmentFactory;
 import resources.ModuleCacheConstants;
 import resources.ModuleCommonCache;
-import resources.utility.ActionHelper;
 import resources.utility.AutomationLogger;
-import resources.utility.ZurpleListingConstants;
 
 /**
  * @author ahabib
@@ -43,6 +39,7 @@ public class ZBORestPostPackage extends RestAPITest{
 		getDriver();
 		lDataFile = pDataFile;
 		HashMap<String,String> ua = new HashMap<String, String>();
+		//TODO This should be initiliazed in RestApi constants
 		ua.put("User-Agent", "aaqib-useragent");
 		JSONObject responseObj = null;
 		String lc_cookie = ModuleCommonCache.getElement(getThreadId(), ModuleCacheConstants.Cookie);
@@ -65,7 +62,8 @@ public class ZBORestPostPackage extends RestAPITest{
 		status = httpCallResp.getJsonResponse().optString("message").equalsIgnoreCase("Success");
 		String lFileToWrite = "/resources/cache/cache-ap-package-id-data.json";
 		String lPFileToWrite = "/resources/cache/permanent-ap-package-admin-data.json";
-		JSONObject jObject = httpCallResp.getJsonResponse();		
+		JSONObject jObject = httpCallResp.getJsonResponse();	
+		//TODO We need to first verify that status code is 200. Than will check status. Line 62 should be after 200 status check.
 		if(status) {
 				String package_id = jObject.optString("package_id").toString();
 				ModuleCommonCache.updateCacheForModuleObject(getThreadId(), ModuleCacheConstants.ZurpleAPPackageId, package_id);
@@ -81,6 +79,7 @@ public class ZBORestPostPackage extends RestAPITest{
 	private RestContent getContent() throws Exception {
 		RestContent restContent = new RestContent();
 		Map<String, Part> multiParts = new HashMap<String, Part>();
+		//TODO Pass this path from datafile
 		String packageDataFile = "/resources/cache/cache-ap-package-data.json";
 		String lName = updateName(dataObject.optString("name"));
 		String lPhone = dataObject.optString("phone");
@@ -91,6 +90,7 @@ public class ZBORestPostPackage extends RestAPITest{
 		String lAdditionalAdmins = dataObject.optString("additional_admins");
 		String lPayers = dataObject.optString("payers");
 		String lSubsidiary = dataObject.optString("subsidiary");
+		//TODO The variable name should be access token since we are already differnt configs files for prod and stage we do not require this check
 		String access_token = getIsProd()?EnvironmentFactory.configReader.getPropertyByName("prod_access_token"):EnvironmentFactory.configReader.getPropertyByName("stage_access_token");
 		
 		multiParts.put("name", new Part(lName, PartType.STRING));
--- src/test/java/com/zurple/backoffice/admin/ZAPackageManagerPageTest.java
+++ src/test/java/com/zurple/backoffice/admin/ZAPackageManagerPageTest.java
@@ -3,7 +3,6 @@ package com.zurple.backoffice.admin;
 import static org.testng.Assert.assertTrue;
 
 import org.json.JSONObject;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.SkipException;
 import org.testng.annotations.AfterTest;
@@ -11,16 +10,10 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.restapi.Part;
-import com.restapi.Part.PartType;
-import com.zurple.admin.ZAAdminManagerPage;
 import com.zurple.admin.ZAPackageManagerPage;
 import com.zurple.my.PageTest;
 
 import resources.AbstractPage;
-import resources.ModuleCacheConstants;
-import resources.ModuleCommonCache;
-import resources.utility.ActionHelper;
 
 /**
  * 
@@ -30,126 +23,127 @@ import resources.utility.ActionHelper;
 
 public class ZAPackageManagerPageTest extends PageTest{
 
-private WebDriver driver;
-private ZAPackageManagerPage page;
-JSONObject dataIdObject;
-JSONObject dataObject;
-String lPackageId = "";
-@Override
-public AbstractPage getPage() {
-	if(page==null) {
-		driver = getDriver();
-		page = new ZAPackageManagerPage(driver);
-		setLoginPage(driver);
-		page.setUrl("");
-		page.setDriver(driver);
+	private WebDriver driver;
+	private ZAPackageManagerPage page;
+	JSONObject dataIdObject;
+	JSONObject dataObject;
+	String lPackageId = "";
+	@Override
+	public AbstractPage getPage() {
+		if(page==null) {
+			driver = getDriver();
+			page = new ZAPackageManagerPage(driver);
+			setLoginPage(driver);
+			page.setUrl("");
+			page.setDriver(driver);
+		}
+		return page;
 	}
-	return page;
-}
-public AbstractPage getPage(String pUrl) {
-	if(page==null) {
-		driver = getDriver();
-		page = new ZAPackageManagerPage(driver);
-		setLoginPage(driver);
-		page.setUrl(pUrl);
-		page.setDriver(driver);
+	public AbstractPage getPage(String pUrl) {
+		if(page==null) {
+			driver = getDriver();
+			page = new ZAPackageManagerPage(driver);
+			setLoginPage(driver);
+			page.setUrl(pUrl);
+			page.setDriver(driver);
+		}
+		return page;
+
 	}
-	return page;
-	
-}
-@Override
-public void clearPage() {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void clearPage() {
+		// TODO Auto-generated method stub
 
-@BeforeTest
-public void backOfficeLogin() {
-	getPage();
-	if(!getLoginPage().doLogin(getZurpeBOUsername(), getZurpeBOPassword())) {
-		throw new SkipException("Skipping the test becasuse [Login] pre-condition was failed.");
 	}
-}
 
-@Test(priority=240)
-@Parameters({"packageIdFile","packageDataFile"})
-public void testSetup(String pPackageIdDataFile, String pPackageDataFile) {
-	dataIdObject = getDataFile(pPackageIdDataFile);
-	dataObject = getDataFile(pPackageDataFile);
-	lPackageId = dataIdObject.optString("package_id");
-	page=null;
-	getPage("/packagemgr/edit/package_id/"+lPackageId); 
-	emptyFile(pPackageIdDataFile,"");
-}
+	@BeforeTest
+	public void backOfficeLogin() {
+		getPage();
+		if(!getLoginPage().doLogin(getZurpeBOUsername(), getZurpeBOPassword())) {
+			throw new SkipException("Skipping the test becasuse [Login] pre-condition was failed.");
+		}
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyFullName() {
-	assertTrue(page.verifyFullName(splitString(dataObject.optString("name"),"content",":","}")),"Unable to verify full name "+lPackageId);
-}
+	@Test(priority=240)
+	@Parameters({"packageIdFile","packageDataFile"})
+	public void testSetup(String pPackageIdDataFile, String pPackageDataFile) {
+		dataIdObject = getDataFile(pPackageIdDataFile);
+		dataObject = getDataFile(pPackageDataFile);
+		lPackageId = dataIdObject.optString("package_id");
+		page=null;
+		getPage("/packagemgr/edit/package_id/"+lPackageId); 
+		emptyFile(pPackageIdDataFile,"");
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyEmail() {
-	assertTrue(page.verifyEmail(splitString(dataObject.optString("email"),"content",":","}")),"Unable to verify email "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyFullName() {
+		//TODO User native jsonobject parsing methods to parse this. This approach is not good.
+		assertTrue(page.verifyFullName(splitString(dataObject.optString("name"),"content",":","}")),"Unable to verify full name "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyPhone() {
-	assertTrue(page.verifyPhone(splitString(dataObject.optString("phone"),"content",":","}")),"Unable to verify phone "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyEmail() {
+		assertTrue(page.verifyEmail(splitString(dataObject.optString("email"),"content",":","}")),"Unable to verify email "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyCompany() {
-	assertTrue(page.verifyCompany(splitString(dataObject.optString("subsidiary"),"content",":","}")),"Unable to verify company "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyPhone() {
+		assertTrue(page.verifyPhone(splitString(dataObject.optString("phone"),"content",":","}")),"Unable to verify phone "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifySetupFee() {
-	assertTrue(page.verifySetupFee(),"Unable to verify setup fee "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyCompany() {
+		assertTrue(page.verifyCompany(splitString(dataObject.optString("subsidiary"),"content",":","}")),"Unable to verify company "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyURLPath() {
-	assertTrue(page.verifyURLPath(splitString(dataObject.optString("path"),"content",":","}")),"Unable to verify url path "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifySetupFee() {
+		assertTrue(page.verifySetupFee(),"Unable to verify setup fee "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyZurpleClientId() {
-	assertTrue(page.verifyZRMClientId(),"Unable to verify client id "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyURLPath() {
+		assertTrue(page.verifyURLPath(splitString(dataObject.optString("path"),"content",":","}")),"Unable to verify url path "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifySubscriptionDate() {
-	assertTrue(page.verifySubscriptionDate(setScheduledPostDate().split(" ")[0]),"Unable to verify subscription date "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyZurpleClientId() {
+		assertTrue(page.verifyZRMClientId(),"Unable to verify client id "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyPayers() {
-	assertTrue(page.verifyPayers(splitString(dataObject.optString("payers"),"content",":","}")),"Unable to verify payers "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifySubscriptionDate() {
+		assertTrue(page.verifySubscriptionDate(setScheduledPostDate().split(" ")[0]),"Unable to verify subscription date "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyAdditionalAdmins() {
-	assertTrue(page.verifyAdditionalAdmins(splitString(dataObject.optString("additional_admins"),"content",":","}")),"Unable to verify additional admins "+lPackageId);
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyPayers() {
+		assertTrue(page.verifyPayers(splitString(dataObject.optString("payers"),"content",":","}")),"Unable to verify payers "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyPackageBundles() { //same as feature bundle
-	assertTrue(page.verifyFeatureBundles("PriorityRanking",2),"Unable to verify feature bundle 1 "+lPackageId); //To be Improved later
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyAdditionalAdmins() {
+		assertTrue(page.verifyAdditionalAdmins(splitString(dataObject.optString("additional_admins"),"content",":","}")),"Unable to verify additional admins "+lPackageId);
+	}
 
-@Test(dependsOnMethods = { "testSetup" })
-public void testVerifyFeatureBundles() {
-	assertTrue(page.verifyFeatureBundles("MassSMS",3),"Unable to verify feature bundle 2 "+lPackageId); //To be Improved later
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyPackageBundles() { //same as feature bundle
+		assertTrue(page.verifyFeatureBundles("PriorityRanking",2),"Unable to verify feature bundle 1 "+lPackageId); //To be Improved later
+	}
 
-public String splitString(String str, String firstSplit, String secondSplit, String thirdSplit) {
-	return str.split(firstSplit)[1].split(secondSplit)[1].split(thirdSplit)[0].replaceAll("^\"|\"$", "");
-}
+	@Test(dependsOnMethods = { "testSetup" })
+	public void testVerifyFeatureBundles() {
+		assertTrue(page.verifyFeatureBundles("MassSMS",3),"Unable to verify feature bundle 2 "+lPackageId); //To be Improved later
+	}
 
-@AfterTest
-public void closeBrowser() {
-	closeCurrentBrowser();
-}
+	public String splitString(String str, String firstSplit, String secondSplit, String thirdSplit) {
+		return str.split(firstSplit)[1].split(secondSplit)[1].split(thirdSplit)[0].replaceAll("^\"|\"$", "");
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		closeCurrentBrowser();
+	}
 
 
 }
