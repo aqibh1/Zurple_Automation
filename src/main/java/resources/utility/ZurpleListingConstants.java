d8943d7bc9ab210b0cc2c457eb1d5650866072ec
aqib.dar@z57.com <aqib.dar@z57.com>
2021-11-16 16:09:45 +0500

    #5525 Fixed listing prod ids and modified the wait time for social post verifications

--- src/main/java/resources/utility/ZurpleListingConstants.java
+++ src/main/java/resources/utility/ZurpleListingConstants.java
@@ -2,11 +2,11 @@ package resources.utility;
 
 public class ZurpleListingConstants {
 	public static final String zurple_staging_listing = "/CA/San_Diego/34112838";
-	public static final String zurple_production_listing = "/CA/San_Diego/62538339";
+	public static final String zurple_production_listing = "/CA/San_Diego/64369611";
 	public static final String zurple_mls_listing_id = "200022516";
 	public static final String zurple_bo_property_url_staging = "/property/34112838";
-	public static final String zurple_bo_property_url_prod = "/property/60372300";
-	public static final String zurple_sapi_listing_id_prod ="61032968";
+	public static final String zurple_bo_property_url_prod = "/property/64369611";
+	public static final String zurple_sapi_listing_id_prod ="64369611";
 	public static final String zurple_sapi_listing_id_stage ="35587661";
 	public static final String zurple_prod_land_url = "https://www.zengtest2.us";
 }
