1f3c5f8b74d40095aea715d79e5614082342393e
aqib.dar@z57.com <aqib.dar@z57.com>
2021-11-23 18:49:16 +0500

    #5567 Added the db methods to fetch the lead id

--- src/test/java/resources/TestEnvironment.java
+++ src/test/java/resources/TestEnvironment.java
@@ -649,5 +649,9 @@ public class TestEnvironment
           return posts;
 	}
 	
+	public User getUserByUserName(String pUserName) {
+		ManageUser manageUser = new ManageUser(getSession());
+		return manageUser.getUserIdByUserName(pUserName);
+	}
 	
 }
