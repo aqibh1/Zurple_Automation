fd60fdcc8934ba224bee20e6547f2cc7e8e113ae
aqib.dar@z57.com <aqib.dar@z57.com>
2021-11-30 15:46:28 +0500

    #5525 Fixed template verification tests

--- src/test/java/com/zurple/backoffice/ZBOCreateTemplatePageTest.java
+++ src/test/java/com/zurple/backoffice/ZBOCreateTemplatePageTest.java
@@ -149,9 +149,9 @@ public class ZBOCreateTemplatePageTest extends PageTest{
 		getPage("/marketing/templates/create");
 		assertTrue(page.clickOnSaveTemplateButton(), "Unable to click on save template button..");
 		ActionHelper.staticWait(5);
-		assertTrue(page.verifyTemplateNameValidationAlertIsTriggered("Please enter a name for your template"), "Template name validation is not triggered");
-		assertTrue(page.verifyTemplateNameValidationAlertIsTriggered("Please enter a subject for your template"), "Template subject validation is not triggered");
-		assertTrue(page.verifyTemplateNameValidationAlertIsTriggered("Please enter template body"), "Template body validation is not triggered");
+		assertTrue(page.verifyTemplateNameValidationAlertIsTriggered("Please enter a name for your template."), "Template name validation is not triggered");
+		assertTrue(page.verifyTemplateNameValidationAlertIsTriggered("Please enter a subject for your template."), "Template subject validation is not triggered");
+		assertTrue(page.verifyTemplateNameValidationAlertIsTriggered("Please enter template body."), "Template body validation is not triggered");
 
 	}
 	/**
