0e2ae848f930f9432d4fb946cc78c2137881d2b2
aqib.dar@z57.com <aqib.dar@z57.com>
2021-12-28 19:45:26 +0500

    #5663 Removed jcabi dependency from POM since it contains old version of log4j

--- src/main/java/resources/SSHConnector.java
+++ src/main/java/resources/SSHConnector.java
@@ -4,41 +4,38 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import com.jcabi.ssh.Shell;
-import com.jcabi.ssh.Ssh;
-import com.jcabi.ssh.SshByPassword;
-
 public class SSHConnector
 {
     public static String runRemoteScript(String command)
     {
-        Shell shell = null;
-        try
-        {
-            ConfigReader configReader = ConfigReader.load();
-            if ( "".equals(configReader.getPropertyByName("ssh_key")))
-            {
-                shell = new SshByPassword(configReader.getPropertyByName("zurple_ssh_host"), Integer.parseInt(configReader.getPropertyByName("zurple_ssh_port")), configReader.getPropertyByName("ssh_user"), configReader.getPropertyByName("ssh_pass"));
-            }else
-            {
-                String privateKey = readKey(configReader.getPropertyByName("ssh_key"));
-
-                if ( "".equals(configReader.getPropertyByName("ssh_pass")))
-                {
-                    shell = new Ssh(configReader.getPropertyByName("zurple_ssh_host"), Integer.parseInt(configReader.getPropertyByName("zurple_ssh_port")), configReader.getPropertyByName("ssh_user"), privateKey);
-                }else
-                {
-                    shell = new Ssh(configReader.getPropertyByName("zurple_ssh_host"), Integer.parseInt(configReader.getPropertyByName("zurple_ssh_port")), configReader.getPropertyByName("ssh_user"), privateKey,configReader.getPropertyByName("ssh_pass"));
-                }
-            }
-
-            String stdout = new Shell.Plain(shell).exec(command);
-            return "OK";
-        }
-        catch (java.io.IOException e)
-        {
-            return e.getMessage();
-        }
+//        Shell shell = null;
+//        try
+//        {
+//            ConfigReader configReader = ConfigReader.load();
+//            if ( "".equals(configReader.getPropertyByName("ssh_key")))
+//            {
+//                shell = new SshByPassword(configReader.getPropertyByName("zurple_ssh_host"), Integer.parseInt(configReader.getPropertyByName("zurple_ssh_port")), configReader.getPropertyByName("ssh_user"), configReader.getPropertyByName("ssh_pass"));
+//            }else
+//            {
+//                String privateKey = readKey(configReader.getPropertyByName("ssh_key"));
+//
+//                if ( "".equals(configReader.getPropertyByName("ssh_pass")))
+//                {
+//                    shell = new Ssh(configReader.getPropertyByName("zurple_ssh_host"), Integer.parseInt(configReader.getPropertyByName("zurple_ssh_port")), configReader.getPropertyByName("ssh_user"), privateKey);
+//                }else
+//                {
+//                    shell = new Ssh(configReader.getPropertyByName("zurple_ssh_host"), Integer.parseInt(configReader.getPropertyByName("zurple_ssh_port")), configReader.getPropertyByName("ssh_user"), privateKey,configReader.getPropertyByName("ssh_pass"));
+//                }
+//            }
+//
+//            String stdout = new Shell.Plain(shell).exec(command);
+//            return "OK";
+//        }
+//        catch (java.io.IOException e)
+//        {
+//            return e.getMessage();
+//        }
+    	return "";
     }
 
     private static String readKey(String filePath)
