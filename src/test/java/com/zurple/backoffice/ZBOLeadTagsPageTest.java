b10795191c55c5bff84b5be76d298e4e1c00f54e
aqib.dar@z57.com <aqib.dar@z57.com>
2022-03-22 12:49:33 +0500

    #5843 Added close browser test

--- src/test/java/com/zurple/backoffice/ZBOLeadTagsPageTest.java
+++ src/test/java/com/zurple/backoffice/ZBOLeadTagsPageTest.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import com.zurple.my.PageTest;
@@ -13,60 +14,64 @@ import resources.utility.AutomationLogger;
 
 
 public class ZBOLeadTagsPageTest extends PageTest{
-WebDriver driver;
-ZBOLeadTagsPage page;
-String updatedTagName = updateName("Auto").replaceAll("\\s+","");
+	WebDriver driver;
+	ZBOLeadTagsPage page;
+	String updatedTagName = updateName("Auto").replaceAll("\\s+","");
 
-@Override
-public AbstractPage getPage() {
-	// TODO Auto-generated method stub
-	if(page==null) {
-		driver = getDriver();
-		page = new ZBOLeadTagsPage(driver);
-		page.setUrl("");
+	@Override
+	public AbstractPage getPage() {
+		// TODO Auto-generated method stub
+		if(page==null) {
+			driver = getDriver();
+			page = new ZBOLeadTagsPage(driver);
+			page.setUrl("");
+		}
+		return page;
 	}
-	return page;
-}
 
-public AbstractPage getPage(String pUrl) {
-	if(page==null) {
-		driver = getDriver();
-		page = new ZBOLeadTagsPage(driver);
-		page.setUrl(pUrl);
-		page.setDriver(driver);
+	public AbstractPage getPage(String pUrl) {
+		if(page==null) {
+			driver = getDriver();
+			page = new ZBOLeadTagsPage(driver);
+			page.setUrl(pUrl);
+			page.setDriver(driver);
+		}
+		return page;
 	}
-	return page;
-}
 
-@Override
-public void clearPage() {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void clearPage() {
+		// TODO Auto-generated method stub
 
-@Test
-public void addRemoveTagFromLeadsCRM() {
-	AutomationLogger.startTestCase("Lead Tags");
-	getPage("/leads/crm");
-	assertTrue(page.createLeadTag(updatedTagName,1), "unable to create a new lead tag");
-	assertTrue(page.addTagConfirmationModal(),"unable to confirm and close confirmation modal");
-	assertEquals(page.tagNameText(), updatedTagName);
-	assertTrue(page.deleteTag(),"unable to delete added tag to a lead");
-	assertTrue(page.removeUsedTag(1),"unable to remove used tag");
-	AutomationLogger.endTestCase();
-}
+	}
 
-@Test
-public void addRemoveTagFromLeadDetails() {
-	AutomationLogger.startTestCase("Lead Tags");
-	getPage("/leads/crm");
-	assertTrue(page.switchToLeadDetailsPage(),"unable to switch to lead details page from lead details");
-	assertTrue(page.createLeadTag(updatedTagName,2),"unable to create a new lead tag from lead details");
-	assertTrue(page.addTagConfirmationModal(),"unable to confirm and close confirmation modal from lead details");
-	assertEquals(page.tagNameText(), updatedTagName);
-	assertTrue(page.deleteTag(),"unable to delete added tag to a lead from lead details");
-	assertTrue(page.removeUsedTag(2),"unable to remove used tag from lead details");
-	AutomationLogger.endTestCase();
-}
+	@Test
+	public void addRemoveTagFromLeadsCRM() {
+		AutomationLogger.startTestCase("Lead Tags");
+		getPage("/leads/crm");
+		assertTrue(page.createLeadTag(updatedTagName,1), "unable to create a new lead tag");
+		assertTrue(page.addTagConfirmationModal(),"unable to confirm and close confirmation modal");
+		assertEquals(page.tagNameText(), updatedTagName);
+		assertTrue(page.deleteTag(),"unable to delete added tag to a lead");
+		assertTrue(page.removeUsedTag(1),"unable to remove used tag");
+		AutomationLogger.endTestCase();
+	}
 
+	@Test
+	public void addRemoveTagFromLeadDetails() {
+		AutomationLogger.startTestCase("Lead Tags");
+		getPage("/leads/crm");
+		assertTrue(page.switchToLeadDetailsPage(),"unable to switch to lead details page from lead details");
+		assertTrue(page.createLeadTag(updatedTagName,2),"unable to create a new lead tag from lead details");
+		assertTrue(page.addTagConfirmationModal(),"unable to confirm and close confirmation modal from lead details");
+		assertEquals(page.tagNameText(), updatedTagName);
+		assertTrue(page.deleteTag(),"unable to delete added tag to a lead from lead details");
+		assertTrue(page.removeUsedTag(2),"unable to remove used tag from lead details");
+		AutomationLogger.endTestCase();
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		closeCurrentBrowser();
+	}
 }
