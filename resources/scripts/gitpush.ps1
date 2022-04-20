1c31024f23e2c6cdf75b9719554ce259766e5379
Muhammad Aaqib Habib <aaqib.habib@z57.com>
2022-04-20 05:12:03 -0700

    updates to gitpush script

--- resources/scripts/gitpush.ps1
+++ resources/scripts/gitpush.ps1
@@ -5,7 +5,7 @@ Write-Host 'Current branch name'
 git branch 
 Write-Host 'git status'
 git status
-write-Host 'pulling..'
+write-Host 'pulling-1..'
 git pull origin sprint
 Write-Host 'Deleting old failed tests xml'
 del C:\FailedTests\z57_selenium\src\test\resources\WorkFlows\zurple\failedTestFormatted.xml
@@ -17,6 +17,8 @@ Write-Host 'Pushing deleted failedTestFormatted.xml'
 git push origin sprint
 Write-Host 'Copying failed tests xml to sprint'
 Copy-item -path C:\Users\Administrator\.jenkins\workspace\Zurple_AutomationFramework_Staging\failedTestFormatted.xml -destination C:\FailedTests\z57_selenium\src\test\resources\WorkFlows\zurple -Force
+write-Host 'pulling-2..'
+git pull origin sprint
 Write-Host 'adding..'
 git add src/test/resources/WorkFlows/zurple/failedTestFormatted.xml
 Write-Host 'commiting..'
